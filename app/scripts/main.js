5d7e38ccfc8c5da53724608139b1ce91992d6f30
Anton Goncharuk <antonge92@gmail.com>
2018-12-04 18:46:20 +0200

    Dev * Animate numbers * Adjust some CSS for logos Box * animate box-shadow for buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('\'Allo \'Allo!');
+
+(() => {
+  const $numbers = $('.js-animate-number');
+  $numbers.each(applyCounterAnimation);
+
+  // taken from https://codepen.io/shivasurya/pen/FatiB
+  function applyCounterAnimation() {
+    const $number = $(this);
+    const propertiesToAnimate = { Counter: $number.text() };
+    const animationOptions = {
+      duration: 2000,
+      easing: 'swing',
+      step: now => $number.text(Math.ceil(now))
+    };
+
+    $number.prop('Counter', 0).animate(propertiesToAnimate, animationOptions);
+  }
+})();
