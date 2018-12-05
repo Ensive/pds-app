18a23d0310b8cf388a5b7a430b42331f3db7b6c1
Anton Goncharuk <antonge92@gmail.com>
2018-12-05 10:28:45 +0200

    Development * Change some copy * Move images * Replace background image * Add greensock lib (for animations) * Update fonts path * Update Header container * Rework some CSS

---
+++ app/scripts/animation.js
@@ -0,0 +1,5 @@
+(() => {
+  // animations
+  TweenMax.to('.js-animate-headline-title', 1, { opacity: 1, x: 0, y: 0 });
+  TweenMax.to('.js-animate-headline-subtitle', 1, { opacity: 1, y: 0 });
+})();
